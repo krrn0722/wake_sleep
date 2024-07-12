7e981c3c986f008b72789e52970a598e226d3d1c
krrn0722 <mur0722@outlook.jp>
2024-07-12 17:24:07 +0900

    生成時のs_k組み合わせをリストにし、すべての組み合わせについて生成するように変更した

--- sample.py
+++ sample.py
@@ -1,47 +1,13 @@
 import numpy as np
 
-ver_check = False
-hor_check = False
+import itertools
 
-def check(arr):
-    ver_check = False
-    hor_check = False
-    # 配列が4x4であることを確認
-    if arr.shape != (4, 4):
-        raise ValueError("配列は4x4でなければなりません")
-    
-    # 各列で1が4つ揃っているかチェック
-    for col in range(4):
-        if np.all(arr[:, col] == 1):
-            ver_check = True
-            break
-    for row in range(4):
-        if np.all(arr[row, :] == 1):
-            hor_check = True
-            break
-    return ver_check , hor_check
+# 10ビットのすべての組み合わせを生成
+combinations = list(itertools.product([0, 1], repeat=10))
 
-# テスト用の配列
-test_arr = np.array([
-    [0, 1, 0, 1],
-    [1, 1, 1, 1],
-    [0, 0, 1, 1],
-    [1, 1, 0, 1]
-])
+# 各組み合わせをnumpy配列に変換し、リストに格納
+s_k_list = [np.array([list(comb)]) for comb in combinations]
 
-both_t_count = 0
-ver_count = 0
-hor_count = 0
-both_f_count = 0
-
-ver_check, hor_check = check(test_arr)
-if(ver_check and hor_check):
-    both_t_count += 1
-elif(ver_check):
-    ver_count += 1
-elif(hor_check):
-    hor_count += 1
-else:
-    both_f_count += 1
-    
-print("どっちも:",both_t_count,"\n","縦:", ver_count, "\n" ,"横:", hor_count,"\n" ,"なし:",both_f_count)
+# 確認のために最初のいくつかを出力
+for item in s_k_list[:5]:
+    print(item)
