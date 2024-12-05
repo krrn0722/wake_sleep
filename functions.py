7fbaf398a4cf3b51fdaebc10b9d14d632f9dc8fa
krrn0722 <mur0722@outlook.jp>
2024-12-05 14:43:49 +0900

    重みを正に制限

--- functions.py
+++ functions.py
@@ -5,6 +5,8 @@ def sigmoid(x):
     return np.exp(np.minimum(x, 0)) / (1 + np.exp(- np.abs(x)))
 
 def sigmoid_tensor(x):
-    x = torch.clip(x, -500, 500)
     return 1 / (1 + torch.exp(-x))
     return torch.exp(torch.minimum(x, torch.zeros_like(x))) / (1 + torch.exp(- torch.abs(x)))
+
+def relu(weight):
+    return weight * (weight > 0).float()
